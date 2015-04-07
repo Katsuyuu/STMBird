ae7edc8f7a15018dc4fdd4d970b26f731a61ceb9
Jedrzej <b4nioo@gmail.com>
2015-04-07 23:38:07 +0200

    Definitions of classes, specified modules.

---
+++ desktop/src/com/stmbird/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stmbird.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stmbird.game.STMBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new STMBird(), config);
+	}
+}
